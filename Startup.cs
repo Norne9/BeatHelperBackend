be0b258a892c17c0264cff7ace31204249421e9f
Aleksey Pestov <dreamport.vr.2@mail.ru>
2020-09-22 12:17:26 +0500

    Use Forwarded Headers

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using BeatHelperBackend.Data;
 using BeatHelperBackend.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -47,6 +48,11 @@ namespace BeatHelperBackend
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
 
